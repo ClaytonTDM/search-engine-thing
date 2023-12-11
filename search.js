769e9c0c0f02b053642aa878d45bd6be2678c673
ClaytonTDM <claytontdm@gmail.com>
2023-12-10 21:25:12 -0500

    done

--- search.js
+++ search.js
@@ -61,4 +61,4 @@ app.get('/search', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // hope this works
